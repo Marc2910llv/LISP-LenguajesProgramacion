cc36c87dd3dc8977e8615f2da7ed3d811ff34485
enVives <perejoanvm@gmail.com>
2023-03-31 13:42:41 +0200

    informacióCercles

--- FuncionsPractica.lisp
+++ FuncionsPractica.lisp
@@ -1,6 +1,77 @@
 ;; FUNCIONS
 
 ;; Guardar dins les propietats d'un àtom simbòlic "spiro" informació per inicialitzar el nostre spirograph
+;;primer hem de fer les llistes de anells grans i petits.
+;;cada anell gran tendrà dues propietats : interiors i exteriors.
+;;cada anell petit tendrà 3 propietats: dents, forats i diàmetre.
+
+(putprop 'anell1 150 'exteriors)
+(putprop 'anell1 105 'interiors)
+(putprop 'anell2 144 'exteriors)
+(putprop 'anell2 96 'interiors)
+
+(putprop 'cercle1 84 'dents)
+(putprop 'cercle1 35 'forats)
+(putprop 'cercle1 56 'diametre)
+
+(putprop 'cercle2 80 'dents)
+(putprop 'cercle2 33 'forats)
+(putprop 'cercle2 53 'diametre)
+
+(putprop 'cercle3 75 'dents)
+(putprop 'cercle3 31 'forats)
+(putprop 'cercle3 50 'diametre)
+
+(putprop 'cercle4 72 'dents)
+(putprop 'cercle4 29 'forats)
+(putprop 'cercle4 48 'diametre)
+
+(putprop 'cercle5 63 'dents)
+(putprop 'cercle5 25 'forats)
+(putprop 'cercle5 42 'diametre)
+
+(putprop 'cercle6 60 'dents)
+(putprop 'cercle6 23 'forats)
+(putprop 'cercle6 40 'diametre)
+
+(putprop 'cercle7 56 'dents)
+(putprop 'cercle7 21 'forats)
+(putprop 'cercle7 37 'diametre)
+
+(putprop 'cercle8 52 'dents)
+(putprop 'cercle8 19 'forats)
+(putprop 'cercle8 35 'diametre)
+
+(putprop 'cercle9 48 'dents)
+(putprop 'cercle9 17 'forats)
+(putprop 'cercle9 32 'diametre)
+
+(putprop 'cercle10 45 'dents)
+(putprop 'cercle10 16 'forats)
+(putprop 'cercle10 30 'diametre)
+
+(putprop 'cercle11 42 'dents)
+(putprop 'cercle11 14 'forats)
+(putprop 'cercle11 28 'diametre)
+
+(putprop 'cercle12 40 'dents)
+(putprop 'cercle12 13 'forats)
+(putprop 'cercle12 27 'diametre)
+
+(putprop 'cercle13 32 'dents)
+(putprop 'cercle13 9 'forats)
+(putprop 'cercle13 21 'diametre)
+
+(putprop 'cercle14 30 'dents)
+(putprop 'cercle14 8 'forats)
+(putprop 'cercle14 20 'diametre)
+
+(putprop 'cercle15 24 'dents)
+(putprop 'cercle15 5 'forats)
+(putprop 'cercle15 16 'diametre)
+
+;;ara ja tenim les dades guardades en els diferents anells i cercles
+
 (defun guarda-informacio (g p rg rp pu in esc int xco yco pa)
     (putprop 'spiro g 'grans)
     (putprop 'spiro p 'petits)
@@ -14,6 +85,12 @@
     (putprop 'spiro yco 'y)
     (putprop 'spiro pa 'pas)
 )
+;;com cridarem guarda-informació:
+(guarda-informacio '(anell1 anell2)
+    '(cercle1 cercle2 cercle3 cercle4 cercle5
+     cercle6 cercle7 cercle8 cercle9 cercle10
+     cercle11 cercle12 cercle13 cercle14 cercle15)
+    150 50 3 40 1.8 t 50 50 0.2)
 
 ;; Colors de la tinta
 (defun vermell ()
